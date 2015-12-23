11846eb2c8f45184a0982177ce6e94e389d0001a
Yixiao Sun <alvinsunyixiao@gmail.com>
2015-12-24 00:41:35 +0800

    bootstrap supported

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res) {
 	var hour = dt.getHours();
 	var minute = dt.getMinutes();
 	minute = minute + hour*60;
-	if (day==4 || day==5 || (day==6 && minute<478)) {
+	if (day==2 || day==3 || day==4 || day==5 || (day==6 && minute<478)) {
 		res.redirect('/normalview');
 		return;
 	}
